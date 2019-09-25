fcb29e78d36f0c044a648b4ed5646365ab0a075b
Blaide <1017007@student.sae.edu.au>
2019-09-25 17:11:57 +1000

    Cool animations on the weapon thing

---
+++ Assets/Scripts/ForceGun/FrontRingAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontRingAnimation : MonoBehaviour
+{
+    public float baseRotationSpeed;
+    public float speedChangeSpeed;
+    public float speedChangeAmount;
+    public bool rotationEnabled;
+
+    public bool pulsateEnabled;
+    public float pulseSpeed;
+    public float pulseScale;
+    public float curScale;
+    public float minScale;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotationEnabled)
+        {
+            Rotate();
+        }
+
+        if (pulsateEnabled)
+        {
+            Pulsate();
+        }
+
+    }
+    void Rotate()
+    {
+        transform.Rotate(0, 0, Mathf.PerlinNoise(1,Time.time *speedChangeSpeed ) *speedChangeAmount+ baseRotationSpeed);
+
+    }
+
+    void Pulsate()
+    {
+        curScale = Mathf.PerlinNoise(0.1f, Time.time * pulseSpeed)*pulseScale +minScale;
+        transform.localScale = new Vector3(curScale,curScale,curScale);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     public float maxSpeed;
 
     private Vector3 moveDir;
+    public float jumpForce;
+
     void Start()
     {
         charCtrlr = GetComponent<CharacterController>();
@@ -28,38 +30,32 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDir.z +=  Input.GetAxis("Vertical") * speed * Time.deltaTime;
         }
-        else if (moveDir.z > 0.001 || moveDir.z < -0.001)
+        else if (charCtrlr.isGrounded && (moveDir.z > 0.001 || moveDir.z < -0.001))
         {
             moveDir.z -= moveDir.z * friction * Time.deltaTime;
         }
-        else
-        {
-            moveDir.z = 0;
-        }
+
 
         if (Mathf.Abs(Input.GetAxis("Horizontal")) >= inputFilter &&  charCtrlr.velocity.magnitude < maxSpeed)
         {
             moveDir.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
         }
-        else if (moveDir.x > 0.001 || moveDir.x < -0.001)
+        else if ( charCtrlr.isGrounded && (moveDir.x > 0.001 || moveDir.x < -0.001))
         {
             moveDir.x -= moveDir.x * friction  * Time.deltaTime;
         }
-        else
-        {
-            moveDir.x = 0;
-        }
+
 
         if (Input.GetKeyDown(KeyCode.Space) && charCtrlr.isGrounded)
         {
-            moveDir.y += 1f;
+            moveDir.y = jumpForce;
         }
-        else //if (!charCtrlr.isGrounded)
+        else if (!charCtrlr.isGrounded)
         {
            moveDir.y += -0.5f * Time.deltaTime; 
         }
 
-        moveDir =  moveDir ;
+        
         
         charCtrlr.Move(transform.rotation *moveDir);
         
