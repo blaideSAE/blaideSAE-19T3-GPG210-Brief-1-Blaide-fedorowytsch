819865b9b96c31a9fc0c4f12aac104a40257997f
Blaide <1017007@student.sae.edu.au>
2019-10-09 08:14:37 +1100

    Tool object selection stuff.

--- Assets/Scripts/ForceGun/States/NoObjectHeld.cs
+++ Assets/Scripts/ForceGun/States/NoObjectHeld.cs
@@ -2,17 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoObjectHeld : MonoBehaviour
+public class NoObjectHeld : StateBase
 {
-    // Start is called before the first frame update
+    public float maxDistance;
+    //private GameObject lastFocused;
+    private GameObject currentFocused;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void Enter()
     {
+
+    }
+
+    public override void Execute()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+        RaycastHit hit;
         
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, layerMask))
+        {
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            currentFocused = hit.collider.gameObject;
+            ObjectFocus focusScript = currentFocused.GetComponent<ObjectFocus>();
+            if (focusScript != null)
+            {
+                focusScript.GotFocus();
+            }
+
+        }
+        else
+        {
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Use();
+        }
+
+    }
+
+    private void Use()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+
+    public override void Exit()
+    {
+    
     }
 }
